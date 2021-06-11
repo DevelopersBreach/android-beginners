711c3c7c9f241262b104bfd4e8c89327be290af5
rajas <rajashekarraju123@gmail.com>
2021-06-11 10:59:06 +0530

    Added TabLayout to ViewPager2

--- app/src/main/java/com/developersbreach/androidbeginners/MovieAdapter.kt
+++ app/src/main/java/com/developersbreach/androidbeginners/MovieAdapter.kt
@@ -1,6 +1,5 @@
 package com.developersbreach.androidbeginners
 
-import android.content.res.ColorStateList
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- app/src/main/java/com/developersbreach/androidbeginners/view/list/ListFragment.kt
+++ app/src/main/java/com/developersbreach/androidbeginners/view/list/ListFragment.kt
@@ -4,22 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import android.widget.ImageView
-import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
-import com.developersbreach.androidbeginners.Movie
 import com.developersbreach.androidbeginners.MovieAdapter
 import com.developersbreach.androidbeginners.MovieRepo
 import com.developersbreach.androidbeginners.R
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class ListFragment : Fragment() {
-
-    private lateinit var viewPager: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,10 +27,26 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewPager = view.findViewById(R.id.movie_view_pager)
+        val tabLayout: TabLayout = view.findViewById(R.id.movie_tab_layout)
+        val viewPager: ViewPager2 = view.findViewById(R.id.movie_view_pager)
+
         val listData = MovieRepo.data(requireContext())
         val adapter = MovieAdapter(listData, movieItemClickListener)
         viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            for (item in listData) {
+                // To start from 0
+                // We are starting at 1
+                // Diff is +1
+                when (position + 1) {
+                    item.movieId -> {
+                        // Executes only when position == item.movieId
+                        tab.text = item.title
+                    }
+                }
+            }
+        }.attach()
     }
 
     private val movieItemClickListener = MovieAdapter.OnClickListener { movie ->
